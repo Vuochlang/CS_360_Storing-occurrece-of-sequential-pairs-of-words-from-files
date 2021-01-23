39a00b0eb0f3eb41f70fd0b083bd19e41e6ffb55
Vuochlang <72162972+Vuochlang@users.noreply.github.com>
2021-01-22 22:03:17 -0800

    move resize hash table functions to another c file

--- code/hash.c
+++ code/hash.c
@@ -60,72 +60,72 @@ void hashPrint (HashTable* hashTable) {
     }
 }
 
-int searchTable (HashTable start, char* combine) {
-    HashTable current = start;
-    HashTable new = start -> next;
-
-    while (new != NULL) {
-        current = new;
-        if (strcmp(current -> wordPair, combine) == 0) {
-            current -> occurrence += 1;
-            return 0;
-        }
-        new = new -> next;
-    }
-    return -1;
-}
-
-bool needResizeTable (int wordCount) {
-    return ((hashTableSize * 5) < wordCount);
-}
-
-HashTable* resizeHashTable (HashTable *oldTable) {
-    hashTableSize *= 3;
-    HashTable* newTable = hashInit();
-    HashTable current, temp;
-    int index;
-    char words[240];
-    int count;
-
-    for (int i = 0; i < (hashTableSize / 3); i++) {
-        current = oldTable[i] -> next;
-        while (current != NULL) {
-            temp = current -> next;
-            index = hashIndex(current -> wordPair);
-            connectOldToNew(newTable[index], current);
-            current = temp;
-        }
-        free(oldTable[i]);
-    }
-    free(oldTable);
-    return newTable;
-}
-
-void connectOldToNew (HashTable newTable, HashTable oldTable) {
-    if (newTable -> next == NULL) {
-        newTable -> next = oldTable;
-        oldTable -> next = NULL;
-    }
-    else {
-        HashTable temp = newTable -> next;
-        newTable -> next = oldTable;
-        oldTable -> next = temp;
-    }
-}
-
-void printWords(HashTable* hashTable, int top) {
-//    int limit = 0;
-//    if (top != -1)  limit = top;
-//    HashTable temp;
-//    char word[240];
-//    for (int i = 0; i < hashTableSize; i++) {
-//        temp = hashTable[i] -> next;
-//        while (temp != NULL) {
+//int searchTable (HashTable start, char* combine) {
+//    HashTable current = start;
+//    HashTable new = start -> next;
 //
-//            strcpy(word, temp -> wordPair);
-//            printf("(%d) \t%10d %s\n", i, temp -> occurrence, word);
-//            temp = temp -> next;
+//    while (new != NULL) {
+//        current = new;
+//        if (strcmp(current -> wordPair, combine) == 0) {
+//            current -> occurrence += 1;
+//            return 0;
 //        }
+//        new = new -> next;
 //    }
-}
+//    return -1;
+//}
+//
+//bool needResizeTable (int wordCount) {
+//    return ((hashTableSize * 5) < wordCount);
+//}
+//
+//HashTable* resizeHashTable (HashTable *oldTable) {
+//    hashTableSize *= 3;
+//    HashTable* newTable = hashInit();
+//    HashTable current, temp;
+//    int index;
+//    char words[240];
+//    int count;
+//
+//    for (int i = 0; i < (hashTableSize / 3); i++) {
+//        current = oldTable[i] -> next;
+//        while (current != NULL) {
+//            temp = current -> next;
+//            index = hashIndex(current -> wordPair);
+//            connectOldToNew(newTable[index], current);
+//            current = temp;
+//        }
+//        free(oldTable[i]);
+//    }
+//    free(oldTable);
+//    return newTable;
+//}
+//
+//void connectOldToNew (HashTable newTable, HashTable oldTable) {
+//    if (newTable -> next == NULL) {
+//        newTable -> next = oldTable;
+//        oldTable -> next = NULL;
+//    }
+//    else {
+//        HashTable temp = newTable -> next;
+//        newTable -> next = oldTable;
+//        oldTable -> next = temp;
+//    }
+//}
+//
+//void printWords(HashTable* hashTable, int top) {
+////    int limit = 0;
+////    if (top != -1)  limit = top;
+////    HashTable temp;
+////    char word[240];
+////    for (int i = 0; i < hashTableSize; i++) {
+////        temp = hashTable[i] -> next;
+////        while (temp != NULL) {
+////
+////            strcpy(word, temp -> wordPair);
+////            printf("(%d) \t%10d %s\n", i, temp -> occurrence, word);
+////            temp = temp -> next;
+////        }
+////    }
+//}
 
